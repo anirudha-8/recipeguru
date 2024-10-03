56f464c4a1e5a4625198a38d7b1bd760a15554ad
Anirudha <beleanirudha8@gmail.com>
2024-10-04 01:09:20 +0530

    complete 'RecipeGuru' app

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,16 @@ import Link from "next/link";
 export default function Home() {
 	return (
 		<>
-			<h1>Welcome to RecipeGuru App</h1>
-			<Link href={"/recipe-list"}>Explore</Link>
+			<div className="text-center mt-[15rem]">
+				<h1 className="text-gray-900 text-3xl font-bold mb-4">
+					Welcome to RecipeGuru App
+				</h1>
+				<Link href={"/recipe-list"}>
+					<button className="bg-green-400 text-white border-white border-[1px] text-2xl px-4 py-2 rounded-lg">
+						Explore
+					</button>
+				</Link>
+			</div>
 		</>
 	);
 }
